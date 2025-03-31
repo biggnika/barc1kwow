c441804dd3be405f0f15a316dae69f5544bb9fb9
biggnika <lukinanikoana@gmail.com>
2025-03-31 15:16:22 +0900

    Add files via upload

---
+++ массив-домашка/main.js
@@ -0,0 +1,15 @@
+const numbers = [1, 4, 5, 10 ,12 , 13, 17]
+
+function printNumbersGreaterThanTen(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 10) {
+            console.log(numbers[i])
+        } 
+    } 
+}
+printNumbersGreaterThanTen(numbers)
+console.log(numbers)
+
+
+
+
